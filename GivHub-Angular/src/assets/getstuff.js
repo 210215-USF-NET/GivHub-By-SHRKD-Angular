ef0d72fc614ad6031140d4e2351c3add1c996b23
Douglas Richardson <42639716+wyspar@users.noreply.github.com>
2021-03-22 12:19:36 -0400

    tried to fix the unfetched error

--- GivHub-Angular/src/assets/getstuff.js
+++ GivHub-Angular/src/assets/getstuff.js
@@ -1,9 +1,36 @@
 function GetCharity()
 {
-    let charity2Search = document.querySelector('#charity2Search').value;
-    fetch(`http://data.orghunter.com/v1/charitysearch?user_key=153f832c549a150e74121c7c7c40667e&searchTerm=${charity2Search}`,{mode: 'no-cors'})
-        .then(result => result.json())
+    let charity2search = document.querySelector('#charity2Search').value;
+
+    let headers = new Headers();
+
+    headers.append('Content-Type', 'application/json');
+    headers.append('Accept', 'application/json');
+  
+    headers.append('Access-Control-Allow-Origin', '*');
+    headers.append('Access-Control-Allow-Credentials', 'true');
+  
+    headers.append('GET', 'POST', 'OPTIONS');
+  
+  //  headers.append('Authorization', 'Basic ' + base64.encode(username + ":" + password));
+
+    fetch('http://data.orghunter.com/v1/charitysearch?user_key=153f832c549a150e74121c7c7c40667e&searchTerm=${charity2Search}',
+    {mode: 'no-cors',
+    credentials: 'include',
+    method: 'POST',
+    headers: headers})
+        .then(result => {
+            let json = result.json();
+            if (json.status >= 200 && json.status < 300) {
+            
+            } else {
+                return json.then(Promise.reject.bind(Promise));
+            }
+            
+        })
+
         .then(charity => {
+            
             document.querySelector('.charityResult name').forEach(name => name.remove());
             document.querySelector('.charityResult foundation').forEach(article => article.remove());
             let article = document.createElement('name');
