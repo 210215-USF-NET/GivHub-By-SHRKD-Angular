8b77088afff1734732cf6d5cc9a366a0e0e6bd59
Douglas Richardson <42639716+wyspar@users.noreply.github.com>
2021-03-25 12:31:14 -0400

    hmm

--- GivHub-Angular/src/app/app.config.ts
+++ GivHub-Angular/src/app/app.config.ts
@@ -1,10 +1,17 @@
 const { CLIENT_ID, ISSUER, OKTA_TESTING_DISABLEHTTPSCHECK } = process.env;
 import { environment } from '../environments/environment';
+var url = "";
+if(environment.production === true){
+  url = "http://192.168.1.5:8080/callback";
+}else{
+  url = "http://192.168.1.5:8080/callback";
+}
+
 export default {
   oidc: {
     clientId: environment.CLIENT_ID,
     issuer: environment.ISSUER,
-    redirectUri: environment.REDIRECTURI,
+    redirectUri: url,
     scopes: ['openid', 'profile', 'email'],
     pkce: true,
     testing: {
