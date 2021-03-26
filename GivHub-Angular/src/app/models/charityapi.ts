84d214de71f4452998a1deb1cd7ddafd6f8db691
KMurphy-5 <kevincitorios@gmail.com>
2021-03-26 10:09:38 -0700

    charity api model

---
+++ GivHub-Angular/src/app/models/charityapi.ts
@@ -0,0 +1,25 @@
+export interface charityapi
+{
+    ein:string,
+    charityName: string,
+    url: string,
+    donationUrl: string,
+    city: string,
+    state: string,
+    zipCode: string,
+    start: number,
+    rows: number,
+    recordCount: number,
+    score: number,
+    acceptingDonations: 1,
+    category: string,
+    eligibleCd: number,
+    deductibilityCd: number,
+    statusCd: number,
+    website: string,
+    missionStatement: string,
+    parent_ein: number,
+    longitude: string,
+    latitude: string
+
+}
