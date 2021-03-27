0be1bb818caf0fdf4e563cb4a31bdfb813b84fbe
Rich Hakes <rjhakes@yahoo.com>
2021-03-27 05:25:37 -0600

    ordered url?query&key

--- GivHub-Angular/src/app/services/nyt-api.service.ts
+++ GivHub-Angular/src/app/services/nyt-api.service.ts
@@ -20,9 +20,9 @@ export class NytApiService {
   .append('Access-Control-Request-Method', 'POST, GET')
   .append('Access-Control-Allow-Headers','X-Requested-With, Content-Type, Origin, Authorization, Accept, Client-Security-Token, Accept-Encoding, X-Auth-Token, content-type');
   url: string = 'https://api.nytimes.com/svc/search/v2/articlesearch.json'
-  key: string = 'api-key=qgJur0BO2ym1GQzYiAU5IAp3orFiMtLR'
   query: string = 'fq=subject:(%22Nonprofit%20Organizations%22)&page=1&sort=newest'
-
+  key: string = 'api-key=qgJur0BO2ym1GQzYiAU5IAp3orFiMtLR'
+  
   constructor(private http: HttpClient) { }
 
   GetTopArticles(): nytapi[] {
