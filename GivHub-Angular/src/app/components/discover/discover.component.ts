8f38ecc57da53e21a83e4cc49f176dff319e922b
KMurphy-5 <kevincitorios@gmail.com>
2021-03-27 21:31:41 -0700

    addition of an discover page

--- GivHub-Angular/src/app/components/discover/discover.component.ts
+++ GivHub-Angular/src/app/components/discover/discover.component.ts
@@ -1,4 +1,8 @@
+import { category } from './../../models/category';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { CharityAPIService } from '../../services/charity-api.service';
+import { charityapi } from 'src/app/models/charityapi';
 
 @Component({
   selector: 'app-discover',
@@ -6,10 +10,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./discover.component.css']
 })
 export class DiscoverComponent implements OnInit {
-
-  constructor() { }
+  category: any;
+  charitiesapi: charityapi[] =[];
+  constructor(private charityService: CharityAPIService, private router: Router, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.route.queryParams.subscribe(params => {
+      this.category = params['category'];
+    });
+    
+    if(this.category){
+      this.charitiesapi = this.charityService.SearchCharitiesByCategory(this.category);
+    }
   }
 
 }
