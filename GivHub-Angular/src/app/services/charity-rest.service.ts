8a342b878b52301d3b38d22155ce26c554e44927
Douglas Richardson <42639716+wyspar@users.noreply.github.com>
2021-03-28 11:59:40 -0400

    fixed vars in environment

--- GivHub-Angular/src/app/services/charity-rest.service.ts
+++ GivHub-Angular/src/app/services/charity-rest.service.ts
@@ -7,7 +7,7 @@ import { HttpClient , HttpHeaders} from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from "rxjs";
 import { subscription } from '../models/subscription';
-
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -18,7 +18,7 @@ export class CharityRESTService {
     headers: new HttpHeaders(
       {
         'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin': '*/*',
+        'Access-Control-Allow-Origin': '*',
         'Access-Control-Allow-Methods': 'OPTIONS, HEAD, GET, POST, PUT, PATCH, DELETE',
         'Access-Control-Allow-Headers': 'X-Requested-With, Content-Type, Origin, Authorization, Accept, Client-Security-Token, Accept-Encoding, X-Auth-Token, content-type',
 
@@ -26,9 +26,9 @@ export class CharityRESTService {
     )
   }
 
-  locationURL: string = 'https://localhost:44389/api/Location';
-  charityURL: string = "https://localhost:44389/api/Charity";
-  subscriptionURL: string = "https://localhost:44389/api/Subscription"
+  locationURL: string = environment.locationURL;
+  charityURL: string = environment.charityURL;
+  subscriptionURL: string = environment.subscriptionURL;
   constructor( private http: HttpClient){}
 
   location: Location;
@@ -50,5 +50,12 @@ export class CharityRESTService {
   }
   GetCharityById(id:number): Observable<charity>{
     return this.http.get<charity>(`${this.charityURL}/${id}`,this.httpOptions);
+  }
+
+  //makes a user sub, gets called from the search-charity component
+  UserSubscribe(sub: subscription): Observable<subscription>{
+    // console.log(subscription.charityId);
+    console.log(sub);
+    return this.http.post<subscription>(this.subscriptionURL,sub,this.httpOptions);
   }
 }
