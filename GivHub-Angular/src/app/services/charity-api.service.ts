68a78b5ef43bd38bd09126361d5c1fc0758b5cee
Douglas Richardson <42639716+wyspar@users.noreply.github.com>
2021-03-27 22:29:08 -0400

    fixed undefined

--- GivHub-Angular/src/app/services/charity-api.service.ts
+++ GivHub-Angular/src/app/services/charity-api.service.ts
@@ -19,25 +19,25 @@ export class CharityAPIService {
   httpOptions = {
     
   }
-
+  headers = new HttpHeaders()
+  .set('Access-Control-Allow-Origin', '*')
+  .set("Access-Control-Expose-Headers","Content-Encoding, Authorization")
+  .set("Access-Control-Max-Age","700")
+  .set('Content-Type','application/json')
+  .set('Access-Control-Request-Method', 'OPTIONS,GET,PUT,POST,DELETE')
+  .set('Access-Control-Allow-Headers','X-Requested-With, Content-Type, Origin, Authorization, Accept, Client-Security-Token, Accept-Encoding, X-Auth-Token, content-type')
+  .set("Access-Control-Request-Headers","Content-Type")
+  .set("Access-Control-Allow-Credentials","true")
+  requestOptions = {  headers: this.headers};  
 
   url: string = 'https://data.orghunter.com/v1/charitysearch?user_key=153f832c549a150e74121c7c7c40667e'
 
   constructor(private http: HttpClient) { }
   
   SearchCharities(searchTerm: string): charityapi[]{
-    let headers = new HttpHeaders();
-    let headers1 = headers.set('Access-Control-Allow-Origin', '*')
-    .set("Access-Control-Expose-Headers","Content-Encoding, Authorization")
-    .set("Access-Control-Max-Age","700")
-    .set('Content-Type','text/css')
-    .set('Access-Control-Request-Method', 'OPTIONS,GET,PUT,POST,DELETE')
-    .set('Access-Control-Allow-Headers','X-Requested-With, Content-Type, Origin, Authorization, Accept, Client-Security-Token, Accept-Encoding, X-Auth-Token, content-type')
-    .set("Access-Control-Request-Headers","Content-Type")
-    .set("Access-Control-Allow-Credentials","true")
-    const requestOptions = {  headers: headers1};  
+
     const proxyUrl = "https://cors-anywhere.herokuapp.com/"
-    let result = this.http.get<charityArray>(`${proxyUrl}${this.url}&searchTerm=${searchTerm}`, requestOptions);
+    let result = this.http.get<charityArray>(`${proxyUrl}${this.url}&searchTerm=${searchTerm}`, this.requestOptions);
     var newCharityArray:charityapi[] = [];
     result.toPromise().then(data => {
       data.data.forEach(x => {
