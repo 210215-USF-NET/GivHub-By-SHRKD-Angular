b0dc0de837747b5676536eb2ceb13457dd1268d5
Douglas Richardson <42639716+wyspar@users.noreply.github.com>
2021-03-31 13:04:17 -0400

    fixed displaying user charities

--- GivHub-Angular/src/app/components/userCharities/userCharities.component.ts
+++ GivHub-Angular/src/app/components/userCharities/userCharities.component.ts
@@ -14,29 +14,55 @@ import {location } from '../../models/location';
 })
 export class UserCharitiesComponent implements OnInit {
   subscriptions:subscription[]=[];
+  subscription: subscription;
   charities : charity[]=[];
-  constructor(private oktaAuth: OktaAuthService, private charityService: CharityRESTService, private router:Router) { }
+  constructor(private oktaAuth: OktaAuthService, private charityRESTService: CharityRESTService, private router:Router) { }
   email:string;
   async ngOnInit() {
     const userClaims =  await this.oktaAuth.getUser();
     this.email = userClaims.email;
-    this.email= 'hans.mittig@revature.net'
     
-    this.charityService.GetUserSubscription(this.email).subscribe(
-    (result)=>{
-     this.subscriptions= result;}
-    );
-
-    console.log(this.subscriptions);
-    console.log(this.charities);
-    this.subscriptions.forEach(function (element) {
-      this.charities.push(this.charityService.GetCharityById(element.charityId));
-    }); 
+    //Get the user subs
+    let userSubsObserable = this.charityRESTService.GetUserSubscription(this.email);
+    userSubsObserable.toPromise().then(data => {
+      data.forEach(x => {
+        this.subscriptions.push(x);
+      });
+    })
+    //hold up
+    while(this.subscriptions.length == 0){
+      await new Promise(resolve => setTimeout(resolve, 1000));
+    }
+    // console.log(this.subscriptions);
+    // console.log(this.subscriptions.length);
+    let getUserCharities = this.charityRESTService.GetAllCharities();
+    getUserCharities.toPromise().then(data => {
+      data.forEach(x => {
+        if(this.subscriptions.find(y => y.charityId.toString() === x.eid)){
+          this.charities.push(x);
+        }
+      });
+    })
+    // console.log(this.charities);
   }
 
   DisplayCharity(charityName: string){
-    this.router.navigate(['hero-details'], { queryParams: { charity: charityName } });
+    this.router.navigate(['displayCharity'], { queryParams: { charity: charityName } });
   }
-  
+  unSubscribe(eid: any, charityName: string, charity: charity): void{
+    (<HTMLInputElement>document.getElementById(eid)).innerHTML = "Subscribed";
+    (<HTMLInputElement>document.getElementById(eid)).classList.remove("btn-primary");
+    (<HTMLInputElement>document.getElementById(eid)).classList.add("btn-success");
+    this.subscription = {
+      id: 0,
+      email: this.email,
+      charityId: Number(eid)
+    }
+    this.charityRESTService.UserSubscribe(this.subscription).subscribe(
+      (sub) => {
+        alert(`You subscribed to ${charityName}.`);
+      }
+    );
+  }
 
 }
