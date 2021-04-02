cf918388666b77f98c2f322d2fac15c2e2b9f33c
Rich Hakes <rjhakes@yahoo.com>
2021-04-02 03:55:22 -0600

    trying to display charity name in donation history

--- GivHub-Angular/src/app/components/profile-donation-history/profile-donation-history.component.ts
+++ GivHub-Angular/src/app/components/profile-donation-history/profile-donation-history.component.ts
@@ -8,9 +8,9 @@ import { OktaAuthService } from '@okta/okta-angular';
 import { donation } from '../../models/donatin';
 import { charity } from '../../models/charity';
 
-interface Claim {
-  claim: string;
-  value: string;
+interface userDon {
+  charityName: string;
+  donation: donation
 }
 
 @Component({
@@ -20,26 +20,61 @@ interface Claim {
 })
 export class ProfileDonationHistoryComponent implements OnInit {
   email: string;
-  donation: donation;
-  userDons: donation[] = [];
+  donations: donation[];
+  userDon: userDon;
+  userDons: userDon[] = [];
   charity: charity;
   constructor(public oktaAuth: OktaAuthService,private router: Router, 
     private route: ActivatedRoute, private charityRESTService: CharityRESTService,
     private charityAPIService: CharityAPIService) {
+      this.userDon = 
+      {
+        charityName: '',
+        donation:
+        {
+          email: '',
+          amount: 1,
+          charityId: 0
+        }
+      }
 
   }
+
 
   async ngOnInit() {
     const userClaims = await this.oktaAuth.getUser();
     this.email = userClaims.email;
-    console.log(this.email);
-
+    let userDonsObservable = this.charityRESTService.GetUserDonations(this.email);
     this.charityRESTService.GetUserDonations(this.email).subscribe(
       (result) => {
-        this.userDons = result;
+
+        this.donations = result;
+        this.donations.sort(function(a, b){return b.amount-a.amount}); 
+        this.donations.forEach(x => {
+            this.assignUserDon(x);
+            this.pushUserDon(this.userDon);
+            
+            })
+          })
+          console.log(this.userDons);
       }
-    )
+      //
+      
+
+  assignUserDon(x: donation) {
+        
+        let getCharity = this.charityRESTService.GetCharityById(x.charityId);
+        getCharity.toPromise().then(y => {
+          this.userDon.donation = x;
+          this.userDon.charityName = y.name;
+          console.log(x);
+          console.log(y);
+        });
+}
+  pushUserDon(userDon: userDon) {
+    this.userDons.push(this.userDon);
   }
+
   // TODO: FIXME
   GetCharityById(ein: any) {
     return this.charityAPIService.GetCharityById(Number(ein));
